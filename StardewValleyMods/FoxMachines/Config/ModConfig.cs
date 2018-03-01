b70a2b074035183701bcf357505c83678037b416
Fox536 <ultimate_X_25@yahoo.com>
2018-02-28 23:49:30 -0600

    1.0.7

--- StardewValleyMods/FoxMachines/Config/ModConfig.cs
+++ StardewValleyMods/FoxMachines/Config/ModConfig.cs
@@ -8,22 +8,41 @@ namespace Fox536.Machines.Config
 {
 	internal class ModConfig
 	{
+		public bool ChangeSprinklers { get; set; } = false;
+		public List<SprinklerArea> SprinklerAreaChanges { get; set; } = new List<SprinklerArea>() { new SprinklerArea() };
+
 		public List<SprinklerScarecrow> SprinklerScarecrowConfig { get; set; } = new List<SprinklerScarecrow>()
 		{
-			new SprinklerScarecrow("Mystic Scarecrow", 13)
+			new SprinklerScarecrow("Mystic Scarecrow", 8)
 		};
 		public List<SpeedIncreaser> SpeedIncreaserConfig { get; set; } = new List<SpeedIncreaser>
 		{
-			new SpeedIncreaser("Temporal Crystal", 50, 5),
-			new SpeedIncreaser("Large Temporal Crystal", 100, 5)
+			new SpeedIncreaser("Small Temporal Crystal", 50, 5),
+			new SpeedIncreaser("Temporal Crystal", 100, 5),
+			new SpeedIncreaser("Large Temporal Crystal", 200, 5)
 		};
+	}
+
+
+	internal class SprinklerArea
+	{
+		public string Name		{ get; set; } = "Sprinkler";
+		public string AreaType	{ get; set; } = "Basic";
+		public int SquareWidth	{ get; set; } = 3;
+		public int CustomNorth	{ get; set; } = 1;
+		public int CustomWest	{ get; set; } = 1;
+		public int CustomEast	{ get; set; } = 1;
+		public int CustomSouth	{ get; set; } = 1;
+
+		public SprinklerArea() { }
 	}
 
 	internal class SprinklerScarecrow
 	{
-		public string	Name  { get; set; }
-		public int		Width { get; set; }
+		public string	Name  { get; set; } = "";
+		public int		Width { get; set; } = 0;
 
+		internal SprinklerScarecrow() { }
 		internal SprinklerScarecrow(string name, int width)
 		{
 			Name = name;
@@ -32,10 +51,11 @@ namespace Fox536.Machines.Config
 	}
 	internal class SpeedIncreaser
 	{
-		public string	Name		{ get; set; }
-		public int		SpeedBuff	{ get; set; }
-		public int		Width		{ get; set; }
+		public string	Name		{ get; set; } = "";
+		public int		SpeedBuff	{ get; set; } = 50;
+		public int		Width		{ get; set; } = 5;
 
+		internal SpeedIncreaser() { }
 		internal SpeedIncreaser(string name, int speedBuff, int width)
 		{
 			Name = name;
--- StardewValleyMods/FoxMachines/ModEntry.cs
+++ StardewValleyMods/FoxMachines/ModEntry.cs
@@ -24,12 +24,12 @@ namespace Fox536.Machines
 		/// <param name="helper">Provides simplified APIs for writing mods.</param>
 		public override void Entry(IModHelper helper)
 		{
+			config = helper.ReadConfig<Config.ModConfig>();
+
 			TimeEvents.AfterDayStarted  += TimeEvents_AfterDayStarted;
 			TimeEvents.TimeOfDayChanged += TimeEvents_TimeOfDayChanged;
-
-			config = helper.ReadConfig<Config.ModConfig>();
 		}
-
+		
 		//-------------------------/
 		#region - Events
 		//-------------------------/
@@ -41,15 +41,21 @@ namespace Fox536.Machines
 			else
 				timePassed = e.NewInt - e.PriorInt;
 
-			foreach (GameLocation location in Game1.locations)
+			try
 			{
-				ItterateMachines(location, false, timePassed);
-			}
+				foreach (GameLocation location in Game1.locations)
+				{
+					ItterateMachines(location, false, timePassed);
+				}
 
-			// Bee Hive Coding
-			foreach (StardewValley.Buildings.Building bLocation in Game1.getFarm().buildings)
+				foreach (StardewValley.Buildings.Building bLocation in Game1.getFarm().buildings)
+				{
+					ItterateMachines(bLocation.indoors, false, timePassed);
+				}
+			} catch (Exception ex)
 			{
-				ItterateMachines(bLocation.indoors, false, timePassed);
+				this.Monitor.Log("Fox ERROR: " + ex.Message, LogLevel.Error);
+				this.Monitor.Log("Fox ERROR: " + ex.Source, LogLevel.Error);
 			}
 		}
 		private void TimeEvents_AfterDayStarted(object sender, EventArgs e)
@@ -58,43 +64,43 @@ namespace Fox536.Machines
 			{
 				foreach (KeyValuePair<Vector2, StardewValley.Object> item in location.objects)
 				{
-					Config.SprinklerScarecrow sprinkler = config.SprinklerScarecrowConfig.Find(x => x.Name == item.Value.name);
+					c.SprinklerScarecrow sprinkler = config.SprinklerScarecrowConfig.Find(x => x.Name == item.Value.name);
 					if (sprinkler != null)
 					{
 						WaterScareCrowArea(location, item.Key, sprinkler);
 					}
+
+					if (config.ChangeSprinklers)
+					{
+						c.SprinklerArea sprinklerArea = config.SprinklerAreaChanges.Find(x => x.Name == item.Value.name);
+						if (sprinklerArea != null)
+						{
+							WaterSprinklerArea(location, item.Key, sprinklerArea);
+						}
+					}
 				}
 			}
 		}
+
+		
 		#endregion
 		//-------------------------/
 
 		//-------------------------/
 		#region Private Methods
 		//-------------------------/
-		private void WaterScareCrowArea(GameLocation location, Vector2 centerPoint, Config.SprinklerScarecrow sprinkler)
+		private void WaterScareCrowArea(GameLocation location, Vector2 centerPoint, c.SprinklerScarecrow sprinkler)
 		{
-			Vector2 currentPoint = new Vector2(centerPoint.X, centerPoint.Y);
+			WaterAreaCircle(location, centerPoint, sprinkler.Width);
+			return;
+
+			Vector2 currentPoint = centerPoint;// new Vector2(centerPoint.X, centerPoint.Y);
 
 			int width = sprinkler.Width;
 			int halfWidth = ((width - 1) / 2);
 
 			// Main Section
-			for (int x = 0; x < width; x++)
-			{
-				for (int y = 0; y < width; y++)
-				{
-					currentPoint.X = centerPoint.X - halfWidth + x;
-					currentPoint.Y = centerPoint.Y - halfWidth + y;
-					if (location.terrainFeatures.ContainsKey(currentPoint))
-					{
-						if (location.terrainFeatures[currentPoint] is HoeDirt)
-						{
-							(location.terrainFeatures[currentPoint] as HoeDirt).state = 1;
-						}
-					}
-				}
-			}
+			WaterAreaSquare(location, centerPoint, 6);
 
 			// West Section 1
 			for (int y = 0; y < width - 2; y++)
@@ -198,8 +204,66 @@ namespace Fox536.Machines
 					}
 				}
 			}
+			
 		}
-		private void SquareSprinkler(Vector2 centerPoint, GameLocation location, int width)
+		private void WaterSprinklerArea(GameLocation location, Vector2 centerPoint, c.SprinklerArea sprinklerArea)
+		{
+			if (sprinklerArea.AreaType.ToLower() == "basic")
+			{
+				List<Vector2> area = new List<Vector2>();
+				area = Utility.getAdjacentTileLocations(centerPoint);
+				WaterArea(location, area);
+			}
+			else if (sprinklerArea.AreaType.ToLower() == "custom")
+			{
+				List<Vector2> oldArea = new List<Vector2>();
+				List<Vector2> area = new List<Vector2>();
+
+				// West
+				Vector2 currentPoint = new Vector2(centerPoint.X, centerPoint.Y);
+				for (int x = 0; x < sprinklerArea.CustomEast; x++)
+				{
+					currentPoint = new Vector2();
+					currentPoint.X = centerPoint.X - 1 - x;
+					currentPoint.Y = centerPoint.Y;
+					area.Add(currentPoint);
+				}
+				// East
+				for (int x = 0; x < sprinklerArea.CustomEast; x++)
+				{
+					currentPoint = new Vector2();
+					currentPoint.X = centerPoint.X + 1 + x;
+					currentPoint.Y = centerPoint.Y;
+					area.Add(currentPoint);
+				}
+				// North
+				for (int y = 0; y < sprinklerArea.CustomNorth; y++)
+				{
+					currentPoint = new Vector2();
+					currentPoint.X = centerPoint.X;
+					currentPoint.Y = centerPoint.Y - 1 - y;
+					area.Add(currentPoint);
+				}
+				// South
+				for (int y = 0; y < sprinklerArea.CustomSouth; y++)
+				{
+					currentPoint = new Vector2();
+					currentPoint.X = centerPoint.X;
+					currentPoint.Y = centerPoint.Y + 1 + y;
+					area.Add(currentPoint);
+				}
+
+				oldArea = Utility.getAdjacentTileLocations(centerPoint);
+				WaterArea(location, oldArea, true);
+				WaterArea(location, area);
+			}
+			else if (sprinklerArea.AreaType.ToLower() == "square")
+			{
+				WaterAreaSquare(location, centerPoint, sprinklerArea.SquareWidth);
+			}
+		}
+
+		private void WaterAreaSquare(GameLocation location, Vector2 centerPoint, int width)
 		{
 			Vector2 currentPoint = new Vector2(centerPoint.X, centerPoint.Y);
 			int halfWidth = ((width - 1) / 2);
@@ -221,6 +285,120 @@ namespace Fox536.Machines
 				}
 			}
 		}
+		private void WaterArea(GameLocation location, List<Vector2> area, bool dry = false)
+		{
+			int state = 1;
+			if (dry) { state = 0; }
+			foreach (var point in area)
+			{
+				if (location.terrainFeatures.ContainsKey(point))
+				{
+					if (location.terrainFeatures[point] is HoeDirt)
+					{
+						(location.terrainFeatures[point] as HoeDirt).state = state;
+					}
+				}
+			}
+		}
+		private void WaterAreaCircle(GameLocation location, Vector2 centerPoint, int width)
+		{
+			int fullWidth = width * 2 + 1;
+			int taper = 4;
+			
+			// Get the area for the Sprinkler
+			List<Vector2> area = new List<Vector2>();
+			Vector2 point = new Vector2();
+			// Main Center Square
+			for (int x = 0; x < fullWidth - taper; x++)
+			{
+				for (int y = 0; y < fullWidth - taper; y++)
+				{
+					point = new Vector2();
+					point.X = centerPoint.X - width + x + taper / 2;
+					point.Y = centerPoint.Y - width + y + taper / 2;
+					area.Add(point);
+				}
+			}
+
+
+
+			int moddedWidth = fullWidth - taper - 2;
+
+			int offset = 0;
+			// West Section
+			for (int x = 1; x < taper / 2 + 1; x++)
+			{
+				for (int y = 0; y < moddedWidth; y++)
+				{
+					if (y > (offset - 1) && y < moddedWidth - offset)
+					{
+						point = new Vector2();
+						point.X = centerPoint.X - (width - taper / 2) - x;
+						point.Y = centerPoint.Y - (moddedWidth - 1) / 2 + y;
+						area.Add(point);
+					}
+				}
+				offset = 1;
+			}
+
+			offset = 0;
+			// East Section
+			for (int x = 1; x < taper / 2 + 1; x++)
+			{
+				for (int y = 0; y < moddedWidth; y++)
+				{
+					if (y > (offset - 1) && y < moddedWidth - offset)
+					{
+						point = new Vector2();
+						point.X = centerPoint.X + (width - taper / 2) + x;
+						point.Y = centerPoint.Y - (moddedWidth - 1) / 2 + y;
+						area.Add(point);
+					}
+				}
+				offset = 1;
+			}
+
+
+
+			offset = 0;
+			// North Section
+			for (int y = 1; y < taper / 2 + 1; y++)
+			{
+				for (int x = 0; x < moddedWidth; x++)
+				{
+				
+					if (x > (offset - 1) && x < moddedWidth - offset)
+					{
+						point = new Vector2();
+						point.X = centerPoint.X - (moddedWidth - 1) / 2 + x;
+						point.Y = centerPoint.Y - (width - taper / 2) - y;
+						area.Add(point);
+					}
+				}
+				offset = 1;
+			}
+
+			offset = 0;
+			// South Section
+			for (int y = 1; y < taper / 2 + 1; y++)
+			{
+				for (int x = 0; x < moddedWidth; x++)
+				{
+					if (y > (offset - 1) && y < moddedWidth - offset)
+					{
+						point = new Vector2();
+						point.X = centerPoint.X - (moddedWidth - 1) / 2 + x;
+						point.Y = centerPoint.Y + (width - taper / 2) + y;
+						area.Add(point);
+					}
+				}
+				offset = 1;
+			}
+
+
+			// Finally Water the area obtained
+			WaterArea(location, area);
+		}
 
 		private void ItterateMachines(GameLocation location, bool isBuildableLocation, int timePassed)
 		{
@@ -241,19 +419,21 @@ namespace Fox536.Machines
 				}
 				
 				// Machine Speed Boost
-				c.SpeedIncreaser speedIncreaser = config.SpeedIncreaserConfig.Find(x => x.Name == machine.Name);
+				c.SpeedIncreaser speedIncreaser = config.SpeedIncreaserConfig.Find(x => x.Name == machine.name);
 				if (speedIncreaser != null)
 				{
 					TemporalIncreaser(location, speedIncreaser, machine, timePassed);
 					continue;
 				}
 
+				/*
 				if (machine.name == "Large Growing Tree")
 				{
 					if (machine.readyForHarvest)
 						if (machine.heldObject != null)
 							print("Machine holds: " + machine.heldObject.name);
 				}
+				*/
 			}
 		}
 
